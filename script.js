f8b44cb5049e09590163ba45593cdfab8afc7689
Ajit Garg <ajit.garg08@gmail.com>
2025-08-09 23:58:49 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const codeToShow = `
+<body>
+    <h2>Revision Project</h2>
+    <p>This project is designed to help you revise key concepts.</p>
+</body>`;
+
+// Find the <code> element and set its text content.
+document.getElementById("code-block").textContent = codeToShow;
